d0759f32b0d51af1c0d316be3b5da7ea50b99321
Taco Ditiecher <taco@tdev.org>
2015-04-21 10:10:52 +0200

    Initial project layout.

---
+++ Geomatics.Tests/EnvironmentTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Geomatics.Tests
+{
+    [TestFixture]
+    public class EnvironmentTest
+    {
+        [Test]
+        public void MakeSureNUnitIsWorking()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# geomatics
+# Geomatics
 Experiments in C# to learn about the geomatics domain.
+
+## References
+[OpenGIS Coordinate Transformation Service specification](http://www.opengeospatial.org/standards/ct)
